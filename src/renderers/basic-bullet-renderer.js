41dba93ebb18f6434cc2e6a040f30169dd809ac9
Diego <marquez.diego.e@gmail.com>
2014-08-12 22:08:33 +0100

    Basic bullet drawing

--- src/renderers/basic-bullet-renderer.js
+++ src/renderers/basic-bullet-renderer.js
@@ -13,7 +13,33 @@ define(["path-renderer", "draw"], function(PathRenderer, Draw) {
 			context.save();
 			
 			context.translate(this.width/2, this.height/2);
-			Draw.circle(context, 0, 0, 10, "#FF0000");
+
+			context.strokeStyle = '#FF0000';
+			context.lineWidth = 1;
+			
+			context.beginPath();
+			context.arc(0, 0, 10, -Math.PI/2, Math.PI/2, false);	
+			
+			var startX = 0;
+			var startY = -10;
+			var zigzagSpacing = 2.5;
+			
+			context.moveTo(startX, startY);
+
+			for (var n = 0; n < 8; n++) {
+				var x = null;
+				var y = startY + ((n + 1) * zigzagSpacing);
+
+				if (n % 2 == 0) {
+					x = startX + 7.5;
+				} else {
+					x = startX;
+				}
+
+				context.lineTo(x, y);
+			}
+
+			context.stroke();
 			
 			context.restore();
 		}
@@ -21,4 +47,3 @@ define(["path-renderer", "draw"], function(PathRenderer, Draw) {
 
 	return BasicBulletRenderer;
 });
-
