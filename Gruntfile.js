988602b86949f5f295cbaa0c4eca262b12495408
Diego <marquez.diego.e@gmail.com>
2014-11-18 11:58:46 +0000

    Task to concatenate stylesheets

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,29 +65,77 @@ module.exports = function(grunt) {
           paths: ['styles/less'],
           strictMath: true
         },
-        files: [
-          {
-            expand: true,
-            cwd: 'styles/less/',   
-            src: ['*/style.less'],
-            dest: 'styles/css/tmp/',
-            rename: function(dest, src) {
-              return dest + src.substring(0, src.indexOf('/')) + '.css';
-            },
+        files: [{
+          expand: true,
+          cwd: 'styles/less/',   
+          src: ['*/style.less'],
+          dest: 'generated/css/',
+          rename: function(dest, src) {
+            return dest + src.substring(0, src.indexOf('/')) + '.css';
+          },
+        }]
+      }
+    },
+
+    concat: {
+      generated_sans_main: {
+        files: [{
+          expand: true,
+          cwd: 'generated/css/',
+          src: ['*.css', '!*main.css'],
+          dest: 'styles/css/',
+          rename: function(dest, src) {
+            return dest + 'all_styles.css';
           }
-        ]
+        }]
+      },
+      plain_sans_main: {
+        files: [{
+          expand: true,
+          cwd: 'styles/css/',
+          src: ['*.css', '!*main.css'],
+          dest: 'styles/css/',
+          rename: function(dest, src) {
+            return dest + 'all_styles.css';
+          }
+        }]
+      },
+      append_main: {
+        files: [{
+          expand: true,
+          src: ['styles/css/all_styles.css', 'generated/css/main.css', 'styles/css/main.css'],
+          dest: 'styles/css/',
+          rename: function(dest, src) {
+            return dest + 'all_styles.css';
+          }
+        }]
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'generated/css/',
+          src: ['!(main.css)'],
+          dest: 'styles/css/',
+          rename: function() {
+            return 'all_styles.css';
+          }
+        }]
       }
     }
   });
 
   // Npm goodness
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-open');
   grunt.loadNpmTasks('grunt-merge-json');
   grunt.loadNpmTasks('grunt-file-append');
-  grunt.loadNpmTasks('grunt-downloadfile');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Local tasks
   grunt.loadTasks('tasks');
@@ -98,10 +146,9 @@ module.exports = function(grunt) {
   grunt.registerTask('config', ['create-config']);
   // This task downloads game-builder source code
   grunt.registerTask('framework', ['clean', 'shell:framework']);
-  // Download files which are not available from bower
-  grunt.registerTask('download', ['downloadfile']);
   // Preprocess .less files
   // grunt.registerTask('less', ['less']);
+  grunt.registerTask('concatenate', ['concat:generated_sans_main', 'concat:plain_sans_main', 'concat:append_main']);
   // This task opens index.html
   grunt.registerTask('run', ['open:index']);
   // This task 
@@ -109,7 +156,7 @@ module.exports = function(grunt) {
     // downloads game-builder source
     // generates requirejs configuration
     // generates the local and remote asset map
-  grunt.registerTask('build', ['shell:bower', 'framework', 'download', 'config', 'asset-map']);
+  grunt.registerTask('build', ['shell:bower', 'framework', 'config', 'asset-map']);
 
   // The default task get's all dependencies, generates everything needed and finally opens index.html 
   grunt.registerTask('default', ['build', 'run']);
