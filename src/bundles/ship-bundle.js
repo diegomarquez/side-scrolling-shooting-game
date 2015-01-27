5a0b18689d6942bc6d80ec53a17e2f870e88d1bf
Diego <marquez.diego.e@gmail.com>
2015-01-27 15:56:52 +0000

    Off with testing game objects and configurations

--- src/bundles/ship-bundle.js
+++ src/bundles/ship-bundle.js
@@ -2,17 +2,13 @@ define(function(require) {
 	var commonBundle = require('common-bundle');
 
 	var playerShip = require("player-ship"); 
-	var playerShipOption = require("player-ship-option");
 	var shipRenderer = require("ship-renderer");
-	var shipOptionRenderer = require("ship-option-renderer");
 
 	var ShipBundle = require("bundle").extend({
 		create: function(args) {	
 			this.componentPool.createPool('ship-renderer', shipRenderer);
-			this.componentPool.createPool('ship-option-renderer', shipOptionRenderer);
 			
 			this.gameObjectPool.createDynamicPool('Ship', playerShip);
-			this.gameObjectPool.createDynamicPool('ShipOption', playerShipOption);
 
 			this.componentPool.createConfiguration("ShipColliderCircle", commonBundle.getCircleColliderPoolId())
 				.args({
@@ -20,56 +16,13 @@ define(function(require) {
 					radius:20
 				});
 
-			this.componentPool.createConfiguration("OptionColliderCircle", commonBundle.getCircleColliderPoolId())
-				.args({
-					id:'optionColliderId', 
-					radius:10
-				});
-			
-			this.componentPool.createConfiguration("ShipColliderPolygon", commonBundle.getPolygonColliderPoolId())
-				.args({
-					id:'shipColliderId', 
-					points: [ {x:-20, y:-20}, {x:20, y:-20}, {x:20, y:20}, {x:-20, y:20} ]
-				});
-
-			this.componentPool.createConfiguration("ShipColliderFixedPolygon", commonBundle.getFixedPolygonColliderPoolId())
-				.args({
-					id:'shipColliderId', 
-					points: [ {x:-20, y:-20}, {x:20, y:-20}, {x:20, y:20}, {x:-20, y:20} ]
-				});
-			
 			this.componentPool.createConfiguration("ShipRenderer", 'ship-renderer');
-			this.componentPool.createConfiguration("ShipOptionRenderer", 'ship-option-renderer');
 			
-			this.gameObjectPool.createConfiguration("PlayerShipOption", "ShipOption")
-				.addComponent("OptionColliderCircle")
-				.setRenderer("ShipOptionRenderer")
-				.childOnly();
-
 			this.gameObjectPool.createConfiguration("PlayerShipCircle", "Ship")
 				.args({
-					rotation: 45
-				})
-				.addChild('PlayerShipOption', {x: -50, y:-50})
-				.addChild('PlayerShipOption', {x: 50, y:-50})
-				.addChild('PlayerShipOption', {x: 50, y:50})
-				.addChild('PlayerShipOption', {x: -50, y:50})
+					rotation: 90
+				})		
 				.addComponent('ShipColliderCircle')
-				.setRenderer("ShipRenderer");
-				
-			
-			this.gameObjectPool.createConfiguration("PlayerShipPolygon", "Ship")
-				.args({
-					rotation: 45
-				})
-				.addComponent('ShipColliderPolygon')
-				.setRenderer("ShipRenderer");
-
-			this.gameObjectPool.createConfiguration("PlayerShipFixedPolygon", "Ship")
-				.args({
-					rotation: 45
-				})
-				.addComponent('ShipColliderFixedPolygon')
 				.setRenderer("ShipRenderer");
 		},
 	});
