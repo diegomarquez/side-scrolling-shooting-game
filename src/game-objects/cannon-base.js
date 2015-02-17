baac8e5538724e801c6d8cff94d54893f6307094
Diego <marquez.diego.e@gmail.com>
2015-02-17 00:07:59 +0000

    Use player getter in cannons

--- src/game-objects/cannon-base.js
+++ src/game-objects/cannon-base.js
@@ -1,11 +1,11 @@
-define(["editor-game-object-container"], function(GameObject) {
+define(["editor-game-object-container", "plater-getter"], function(GameObject, PlayerGetter) {
   var Cannon = GameObject.extend({
     init: function() {
       this._super();
     },
 
     editorStart: function() {
-      
+			this.target = PlayerGetter.get();     
     },
 
     editorUpdate: function(delta) {
