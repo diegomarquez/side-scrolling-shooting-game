0d0e132810a1e4a238c9801756aa63e911dc82da
Diego <marquez.diego.e@gmail.com>
2015-02-17 00:08:37 +0000

    Add component to activate object control objects on view

--- src/bundles/control-objects-bundle.js
+++ src/bundles/control-objects-bundle.js
@@ -1,11 +1,17 @@
 define(function(require) {	
+	var commonBundle = require("common-bundle");
+
 	var ControlObjectsBundle = require("bundle").extend({
 		create: function(args) {	
-			this.gameObjectPool.createDynamicPool("ScrollStopper", require('scroll-stopper'));
-			this.gameObjectPool.createConfiguration("ScrollStopper", 'ScrollStopper');
+			this.componentPool.createConfiguration("ActivateControlOnView", commonBundle.getActivateOnViewPoolId());
 
+			this.gameObjectPool.createDynamicPool("ScrollStopper", require('scroll-stopper'));
 			this.gameObjectPool.createDynamicPool("BossWarning", require('boss-warning'));
-			this.gameObjectPool.createConfiguration("BossWarning", 'BossWarning');
+			
+			this.gameObjectPool.createConfiguration("ScrollStopper", 'ScrollStopper')
+				.addComponent("ActivateControlOnView");
+			this.gameObjectPool.createConfiguration("BossWarning", 'BossWarning')
+				.addComponent("ActivateControlOnView");
 		},
 	});
 
