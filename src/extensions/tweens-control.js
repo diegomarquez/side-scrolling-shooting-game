cfadd64960d25f1848f25fcb51ccc95c91ee53a2
Diego <marquez.diego.e@gmail.com>
2015-04-02 23:13:20 +0100

    Extension to pause and resume tweens on blur and focus

---
+++ src/extensions/tweens-control.js
@@ -0,0 +1,26 @@
+define(["extension", "gb", "timelinelite"], function(Extension, Gb, TimelineLite) {
+  var game = Gb.game;
+
+  var TweensControl = Extension.extend({
+    type: function() {
+      return Gb.game.CREATE;
+    },
+
+    execute: function() {
+      game.on(game.BLUR, this, function() {
+				this.tweensTimeline = TimelineLite.exportRoot();
+			  this.tweensTimeline.pause();
+			}, false, false, false, 'tweens');
+
+			game.on(game.FOCUS, this, function() {
+				this.tweensTimeline.resume();
+			}, false, false, false, 'tweens');
+    },
+
+    destroy: function() {
+			game.levelCleanUp('tweens');
+		}
+  });
+
+  return TweensControl;
+});
