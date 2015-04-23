77c5f33563f93b79a6b155c3bd3eba1190c9e4a8
Diego <marquez.diego.e@gmail.com>
2015-04-23 22:54:22 +0100

    Fix drop down scroll

--- src/scene/editor/ui-components/primitives/dropdowns/dropdown-scroll.js
+++ src/scene/editor/ui-components/primitives/dropdowns/dropdown-scroll.js
@@ -18,8 +18,37 @@ define(function(require) {
     	$(contentContainer).jScrollPane({showArrows: true});
     },
 
+    setupOptionEvents: function(optionElements, container, contentContainer, options) {
+      this._super(optionElements, container, contentContainer, options);
+
+      var mainButton = $(container).find('.main-button').find('span')[0];
+
+      var self = this;
+
+      for (var i = 0; i < optionElements.length; i++) {
+        var option = optionElements[i];
+
+        if (!$(option).hasClass('ui-state-default')) {
+          continue;
+        }
+
+        $(option).on('click', function() {
+          $(this).removeClass('ui-state-hover');
+
+          var value = $(this).attr('value');
+
+          mainButton.innerHTML = options.selectedMessage + " " + value;
+          $(container).attr('value', value);
+
+          self.removeMenu(contentContainer);
+        });
+      }
+    },
+
     removeMenu: function(element) {
-    	this._super(element)
+    	this._super(element);
+
+    	this.refreshContent();
       $(element).jScrollPane().data('jsp').destroy();
     }
   });
