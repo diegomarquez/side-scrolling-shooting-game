32d0ad12db391201a4fab013502c0d9a956340bc
Diego <marquez.diego.e@gmail.com>
2015-04-25 17:49:56 +0100

    Improve grid line drawing

--- src/scene/editor/bundles/grid-bundle.js
+++ src/scene/editor/bundles/grid-bundle.js
@@ -25,23 +25,26 @@ define(function(require) {
 
 					drawPath: function(context) {
 						context.save();
+
+						context.translate(0.5, 0.5);
+						context.globalAlpha = 0.5;
 						context.beginPath();
 						
-						for (var i = 1; i < step.width; i++) {
+						for (var i = 1; i < Math.floor(step.width); i++) {
 							var posX = step.width * i;
 	        				
       				context.moveTo(posX, 0);
       				context.lineTo(posX, gb.canvas.height);	
 						}
 
-						for (var i = 1; i < step.height; i++) {
+						for (var i = 1; i < Math.floor(step.height); i++) {
       				var posY = step.height * i;
 
       				context.moveTo(0, posY);
       				context.lineTo(gb.canvas.width, posY);	
 						}
 
-		        context.lineWidth = 0.3;
+		        context.lineWidth = 1;
         		context.strokeStyle = "#f0ad4e";
         		context.stroke();        	
 						context.closePath();
