2466eee8c73c300f5a275ef6d821e1f1dad6e7ed
Diego Enrique Marquez <marquez.diego.e@gmail.com>
2015-07-27 19:04:52 +0100

    Update asset-map.js

--- src/asset-map.js
+++ src/asset-map.js
@@ -2,16 +2,16 @@ define(function() {
 	var Data = function() {};
 
 	var data = {
-	"BALL.PNG": "assets/images/Ball.png",
-	"CHECKEREDFLAG.PNG": "assets/images/CheckeredFlag.png",
-	"FLAG.PNG": "assets/images/Flag.png",
-	"WARNING.PNG": "assets/images/Warning.png",
-	"BIOHAZARD.PNG": "assets/images/biohazard.png",
-	"DOWNARROW.PNG": "assets/images/downArrow.png",
-	"LEFTARROW.PNG": "assets/images/leftArrow.png",
-	"RIGHTARROW.PNG": "assets/images/rightArrow.png",
-	"UPARROW.PNG": "assets/images/upArrow.png",
-	"SPACEINVADERS_FIRE.WAV": "assets/sound/SpaceInvaders_Fire.wav"
+	"BALL.PNG": "assets/images\\Ball.png",
+	"CHECKEREDFLAG.PNG": "assets/images\\CheckeredFlag.png",
+	"FLAG.PNG": "assets/images\\Flag.png",
+	"WARNING.PNG": "assets/images\\Warning.png",
+	"BIOHAZARD.PNG": "assets/images\\biohazard.png",
+	"DOWNARROW.PNG": "assets/images\\downArrow.png",
+	"LEFTARROW.PNG": "assets/images\\leftArrow.png",
+	"RIGHTARROW.PNG": "assets/images\\rightArrow.png",
+	"UPARROW.PNG": "assets/images\\upArrow.png",
+	"SPACEINVADERS_FIRE.WAV": "assets/sound\\SpaceInvaders_Fire.wav"
 };
 
 	Data.prototype.get = function() {
